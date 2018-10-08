211b811b4e8388115679d97b1ca7ee20d552a46e
Josue Exhume <exhumejosue@gmail.com>
2018-10-07 21:08:11 -0400

    Add project files.

---
+++ FacebookClone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FacebookClone.Startup))]
+namespace FacebookClone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
