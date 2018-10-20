87ae59dab67b94bc6a10178ffbf1d01eca107018
Josue Exhume <exhumejosue@gmail.com>
2018-10-20 16:17:17 -0400

    added post model

---
+++ FacebookClone/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FacebookClone.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public Post ParentId { get; set; }
+        public ApplicationUser UserId { get; set; }
+        public string Content { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public DateTime CreatedOn  { get; set; }
+        public DateTime ModifiedOn  { get; set; }
+        public virtual ICollection<Post> ChildPost { get; set; }
+    }
+}
