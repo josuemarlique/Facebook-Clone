46f7d812b812c4026c6f6ea3fb18ae8fa8e0b5fd
Josue Exhume <exhumejosue@gmail.com>
2018-10-20 16:18:04 -0400

    added name, gender, and profil pic property to application user

--- FacebookClone/Models/ApplicationUser.cs
+++ FacebookClone/Models/ApplicationUser.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -8,6 +11,13 @@ namespace FacebookClone.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Post ProfilePicture { get; set; }
+        public Gender Gender { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
