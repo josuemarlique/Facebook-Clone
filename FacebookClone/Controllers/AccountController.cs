ce6aacbffcd466e8ea03a2813e79ebceecd03c94
Josue Exhume <exhumejosue@gmail.com>
2018-10-20 16:15:54 -0400

    added missing import for account model namespace

--- FacebookClone/Controllers/AccountController.cs
+++ FacebookClone/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using FacebookClone.Models;
+using FacebookClone.Models.Account;
 
 namespace FacebookClone.Controllers
 {
