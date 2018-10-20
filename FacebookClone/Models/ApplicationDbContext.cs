8a3c72e4d5b8ed7ba3b1c67c0dd09d030a61e155
Josue Exhume <exhumejosue@gmail.com>
2018-10-20 16:19:13 -0400

    added fluent api config for Gender,Post,and app users

--- FacebookClone/Models/ApplicationDbContext.cs
+++ FacebookClone/Models/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace FacebookClone.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -12,6 +16,48 @@ namespace FacebookClone.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(u => u.Id)
+                .HasColumnName("UserId");
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(u => u.FirstName)
+                .IsRequired();
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(u => u.LastName)
+                .IsRequired();
+            modelBuilder.Entity<ApplicationUser>()
+                .HasOptional(u => u.ProfilePicture)
+                .WithMany()
+                .Map(m => m.MapKey("ProfilePictureId"));
+            modelBuilder.Entity<ApplicationUser>()
+                .HasRequired(u => u.Gender)
+                .WithMany()
+                .Map(m => m.MapKey("GenderId"))
+                .WillCascadeOnDelete(false);
+
+
+            modelBuilder.Entity<Post>()
+                .HasOptional(p => p.ParentId)
+                .WithMany(p => p.ChildPost)
+                .Map(m => m.MapKey("ParentId"))
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Post>()
+                .HasRequired(p => p.UserId)
+                .WithMany()
+                .Map(m => m.MapKey("UserId"));
+
+            modelBuilder.Entity<Gender>()
+                .Property(p => p.Name)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+
         }
     }
 }
