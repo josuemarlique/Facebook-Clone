392074168cc4f298b1c541aeeda3941ba07100d2
Josue Exhume <exhumejosue@gmail.com>
2018-10-27 19:46:09 -0400

    added global filter to restric app to only authorize

--- FacebookClone/Global.asax.cs
+++ FacebookClone/Global.asax.cs
@@ -16,6 +16,7 @@ namespace FacebookClone
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalFilters.Filters.Add(new AuthorizeAttribute());
         }
     }
 }
