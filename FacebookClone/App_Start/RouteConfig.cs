8f9b983512cbe33870b00784b37e80584c9a6dd4
Josue Exhume <exhumejosue@gmail.com>
2018-10-27 19:46:51 -0400

    changed default route config to the register view

--- FacebookClone/App_Start/RouteConfig.cs
+++ FacebookClone/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FacebookClone
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Register", id = UrlParameter.Optional }
             );
         }
     }
