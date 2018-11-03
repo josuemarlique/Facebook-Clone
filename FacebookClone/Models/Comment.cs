12ae64a6344c592a6ac74604c631260773b2a37e
Josue Exhume <exhumejosue@gmail.com>
2018-11-03 19:23:22 -0400

    finished creating template for logged in users, landing page(accidentaly commit all because of Visual studio)

---
+++ FacebookClone/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacebookClone.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        public Comment ParentId { get; set; }
+        public ApplicationUser User { get; set; }
+        public Post Post { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
