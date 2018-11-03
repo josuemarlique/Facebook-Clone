abac63c82bf79e097584e5f38bfca08d36d4a6a9
Josue Exhume <exhumejosue@gmail.com>
2018-11-03 19:21:50 -0400

    added identityextension to get first name

---
+++ FacebookClone/Helpers/IdentityExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace FacebookClone.Helpers
+{
+    public static class IdentityExtension
+    {
+        public static string GetFirstName(this IIdentity identity)
+        {
+            if (identity == null)
+            {
+                throw new ArgumentException("Identity");
+            }
+
+            var claimsIdentity = identity as ClaimsIdentity;
+
+            return claimsIdentity?.FindFirstValue("FirstName");
+        }
+    }
+}
